38284f61fa60939d77d1159fdce390df7f410a5d
Stardust <grasp-embark-spoof@duck.com>
2025-10-03 17:28:19 +0200

    Fix indentation

--- zathura/shortcuts.c
+++ zathura/shortcuts.c
@@ -81,56 +81,56 @@ static bool link_shortcuts(zathura_t* zathura, girara_callback_inputbar_activate
 
 /* Helper function to redo the search and stay in place */
 bool redo_search(zathura_t* zathura){
-    if (zathura->document == NULL || strlen(zathura->global.search_string) == 0) {
-        return false;
+  if (zathura->document == NULL || strlen(zathura->global.search_string) == 0) {
+    return false;
+  }
+
+  zathura_error_t error = ZATHURA_ERROR_OK;
+
+  unsigned int number_of_pages     = zathura_document_get_number_of_pages(zathura->document);
+  unsigned int current_page_number = zathura_document_get_current_page_number(zathura->document);
+
+  /* search pages */
+  for (unsigned int page_id = 0; page_id < number_of_pages; ++page_id) {
+    unsigned int index   = (page_id + current_page_number) % number_of_pages;
+    zathura_page_t* page = zathura_document_get_page(zathura->document, index);
+    if (page == NULL) {
+      continue;
     }
 
-    zathura_error_t error = ZATHURA_ERROR_OK;
+    GtkWidget* page_widget   = zathura_page_get_widget(zathura, page);
+    GObject* obj_page_widget = G_OBJECT(page_widget);
+    g_object_set(obj_page_widget, "draw-links", FALSE, NULL);
 
-    unsigned int number_of_pages     = zathura_document_get_number_of_pages(zathura->document);
-    unsigned int current_page_number = zathura_document_get_current_page_number(zathura->document);
+    zathura_renderer_lock(zathura->sync.render_thread);
+    girara_list_t* result = zathura_page_search_text(page, zathura->global.search_string, &error);
+    zathura_renderer_unlock(zathura->sync.render_thread);
 
-    /* search pages */
-    for (unsigned int page_id = 0; page_id < number_of_pages; ++page_id) {
-        unsigned int index   = (page_id + current_page_number) % number_of_pages;
-        zathura_page_t* page = zathura_document_get_page(zathura->document, index);
-        if (page == NULL) {
-          continue;
-        }
+    if (result == NULL || girara_list_size(result) == 0) {
+      girara_list_free(result);
+      g_object_set(obj_page_widget, "search-results", NULL, NULL);
 
-        GtkWidget* page_widget   = zathura_page_get_widget(zathura, page);
-        GObject* obj_page_widget = G_OBJECT(page_widget);
-        g_object_set(obj_page_widget, "draw-links", FALSE, NULL);
-
-        zathura_renderer_lock(zathura->sync.render_thread);
-        girara_list_t* result = zathura_page_search_text(page, zathura->global.search_string, &error);
-        zathura_renderer_unlock(zathura->sync.render_thread);
-
-        if (result == NULL || girara_list_size(result) == 0) {
-            girara_list_free(result);
-            g_object_set(obj_page_widget, "search-results", NULL, NULL);
-
-            if (error == ZATHURA_ERROR_NOT_IMPLEMENTED) {
-                break;
-            } else {
-                continue;
-            }
-        }
-
-        g_object_set(obj_page_widget, "search-results", result, NULL);
-
-        if (zathura->global.search_direction == BACKWARD) {
-            /* start at bottom hit in page */
-            g_object_set(obj_page_widget, "search-current", girara_list_size(result) - 1, NULL);
-        } else {
-            g_object_set(obj_page_widget, "search-current", 0, NULL);
-        }
+      if (error == ZATHURA_ERROR_NOT_IMPLEMENTED) {
+        break;
+      } else {
+        continue;
+      }
     }
-    if (zathura->global.search_results_highlighted == true) {
-        /* highlight search results */
-        document_draw_search_results(zathura, true);
+
+    g_object_set(obj_page_widget, "search-results", result, NULL);
+
+    if (zathura->global.search_direction == BACKWARD) {
+      /* start at bottom hit in page */
+      g_object_set(obj_page_widget, "search-current", girara_list_size(result) - 1, NULL);
+    } else {
+      g_object_set(obj_page_widget, "search-current", 0, NULL);
     }
-    return true;
+  }
+  if (zathura->global.search_results_highlighted == true) {
+    /* highlight search results */
+    document_draw_search_results(zathura, true);
+  }
+  return true;
 }
 
 bool sc_abort(girara_session_t* session, girara_argument_t* UNUSED(argument), girara_event_t* UNUSED(event),
@@ -562,7 +562,7 @@ bool sc_reload(girara_session_t* session, girara_argument_t* UNUSED(argument), g
 
   /* redo search to preserve the previous search state */
   if(zathura->global.search_string != NULL){
-      return redo_search(zathura);
+    return redo_search(zathura);
   }
 
   return true;
